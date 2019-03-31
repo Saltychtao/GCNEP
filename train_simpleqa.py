9b06045ad9a72b2d17cb27fde8c8bd2b4a4d810d
Saltychtao <lijh@nlp.nju.edu.cn>
2019-03-31 13:01:22 +0800

    add visualize analysis

--- train_simpleqa.py
+++ train_simpleqa.py
@@ -1,11 +1,15 @@
 import torch
-import dgl
-import dill
 import os
 import numpy as np
+from pprint import pprint
+from tensorboardX import SummaryWriter
+from argparse import ArgumentParser
 from torch.utils.data import DataLoader
 from dataloader.simpleQA_dataloader import SimpleQADataset
 from learner.SimpleQA import SimpleQA
+from utils.graph_util import build_graph_from_adj_matrix
+from utils.visualize import plot
+from utils.util import parse_args
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -25,37 +29,19 @@ def main(args):
     import time
     start_time = time.time()
     vocab = SimpleQADataset.load_vocab(args)
-    adj_matrix = torch.load(args.relation_adj_matrix_pth)
-    print('Relation Adj matrix loaded!')
-    num_nodes = len(adj_matrix)
-    print('Total number of relations :{}'.format(num_nodes))
-    src = []
-    dst = []
-    triplets = []
-    for i in range(num_nodes):
-        for j in range(num_nodes):
-            if adj_matrix[i][j] == 1:
-                src.append(i)
-                dst.append(j)
-                triplets.append((i,j))
-    g = dgl.DGLGraph()
-    g.add_nodes(num_nodes)
-    g.add_edges(src,dst)
-
-    def comp_deg_norm(g):
-        in_deg = g.in_degrees(range(g.number_of_nodes())).float().numpy()
-        norm = 1.0 / in_deg
-        norm[np.isinf(norm)] = 0
-        return norm
-    norm = comp_deg_norm(g)
-    node_id = torch.arange(0,num_nodes,dtype=torch.long).view(-1,1).to(device)
-    norm = torch.from_numpy(norm).view(-1,1).to(device)
-    g.ndata.update({'id':node_id,'norm':norm})
-    print('Building Relation Graph ... Done.')
-    args.relation_graph = g
-    args.relation_pretrained = torch.load(args.relation_pretrained_pth)
     end_time = time.time()
     print('Loaded dataset in {:.2f}s'.format(end_time - start_time))
+
+    if args.use_gcn:
+        args.relation_graphs = []
+        for pth in args.relation_adj_matrix_pth:
+            adj_matrix = torch.load(pth)
+            print('Relation Adj matrix loaded!')
+            print('Building Relation Graph ...',end='')
+            g = build_graph_from_adj_matrix(adj_matrix,device)
+            print('Done.')
+            args.relation_graphs.append(g)
+        args.sub_relation_dim = args.relation_dim // len(args.relation_graphs)
 
     args.n_words = len(vocab.stoi)
     args.n_relations = len(vocab.rtoi)
@@ -75,27 +61,59 @@ def main(args):
         args.relation_pretrained = None
         print(' Using random initialized label word embedding.')
 
-    if args.mode == 'supervised':
-        train_dataset,dev_dataset,test_dataset = SimpleQADataset.load_dataset(args)
-        train(args,train_dataset,dev_dataset,test_dataset,vocab,SimpleQADataset.collate_fn)
-    elif args.mode == 'zero-shot':
-        base_data_dir = args.data_dir
-        base_save_dir = args.save_dir
-        for i in range(10):
+    if not os.path.exists(args.save_dir):
+        os.mkdir(args.save_dir)
+
+    base_data_dir = args.data_dir
+    base_save_dir = args.save_dir
+
+    for i in range(args.fold):
+        train_fname = os.path.join(base_data_dir,'fold-{}'.format(i),'train.tsv')
+        dev_fname = os.path.join(base_data_dir,'fold-{}'.format(i),'dev.tsv')
+        test_fname = os.path.join(base_data_dir,'fold-{}'.format(i),'test.tsv')
+        train_dataset,dev_dataset,test_dataset = SimpleQADataset.load_dataset(train_fname,dev_fname,test_fname,args.vocab_pth,args)
+        args.save_dir = os.path.join(base_save_dir,'fold-{}'.format(str(i)))
+        args.log_dir = os.path.join(args.save_dir,'logwriter')
+        args.writer = SummaryWriter(log_dir=args.log_dir)
+
+        train_relations = train_dataset.get_label_set()
+        args.seen_idx = list(train_relations)
+        args.unseen_idx = list(set([i  for i in range(len(vocab.rtoi))]) - set(args.seen_idx))
+        label_idx = np.zeros(len(vocab.rtoi))
+        label_idx[args.seen_idx] = 1
+
+        if args.train:
             print(' Training Fold {}'.format(i))
-            train_fname = os.path.join(base_data_dir,'fold-{}'.format(i),'train.tsv')
-            dev_fname = os.path.join(base_data_dir,'fold-{}'.format(i),'dev.tsv')
-            test_fname = os.path.join(base_data_dir,'fold-{}'.format(i),'test.tsv')
-            train_dataset,dev_dataset,test_dataset = SimpleQADataset.load_dataset(train_fname,dev_fname,test_fname,args.vocab_pth,args)
-            args.save_dir = os.path.join(base_save_dir,'fold-{}'.format(str(i)))
             train(args,train_dataset,dev_dataset,test_dataset,vocab,SimpleQADataset.collate_fn)
+        elif args.evaluate:
+            with torch.no_grad():
+                print(' Test Fold {}'.format(i))
+                # All
+                test_micro_acc,test_macro_acc = evaluate(args,test_dataset,vocab,SimpleQADataset.collate_fn)
+                print('Test Acc :({:.2f},{:.2f})'.format(test_micro_acc*100,test_macro_acc*100))
+
+                # Seen
+                train_relations = train_dataset.get_label_set()
+                seen_subset = test_dataset.get_subset(train_relations,'seen')
+                seen_micro_acc,seen_macro_acc = evaluate(args,seen_subset,vocab,SimpleQADataset.collate_fn)
+                print('Seen Acc :({:.2f},{:.2f})'.format(seen_micro_acc*100,seen_macro_acc*100))
+
+                # Unseen
+                train_relations = train_dataset.get_label_set()
+                seen_subset = test_dataset.get_subset(train_relations,'unseen')
+                unseen_micro_acc,unseen_macro_acc = evaluate(args,seen_subset,vocab,SimpleQADataset.collate_fn)
+                print('UnSeen Acc :({:.2f},{:.2f})'.format(unseen_micro_acc*100,unseen_macro_acc*100))
+        elif args.visualize:
+            print(' Visualizing Fold {}'.format(i))
+            fname = os.path.join(args.save_dir,'embedding.png')
+            visualize(args,label_idx,vocab,fname)
 
 
 def train(args,train_dataset,dev_dataset,test_dataset,vocab,collate_fn):
 
     train_iter = DataLoader(dataset=train_dataset,batch_size=args.batch_size,shuffle=False,num_workers=12,collate_fn=collate_fn)
-    dev_iter = DataLoader(dataset=dev_dataset,batch_size=args.batch_size,shuffle=True,num_workers=12,collate_fn=collate_fn)
-    test_iter = DataLoader(dataset=test_dataset,batch_size=args.batch_size,shuffle=True,num_workers=12,collate_fn=collate_fn)
+    dev_iter = DataLoader(dataset=dev_dataset,batch_size=32,shuffle=True,num_workers=12,collate_fn=collate_fn)
+    test_iter = DataLoader(dataset=test_dataset,batch_size=32,shuffle=True,num_workers=12,collate_fn=collate_fn)
 
     args.n_words = len(vocab.stoi)
     args.n_relations = len(vocab.rtoi)
@@ -114,118 +132,85 @@ def train(args,train_dataset,dev_dataset,test_dataset,vocab,collate_fn):
     logfile = open(os.path.join(args.save_dir,'log.txt'),'w')
     for epoch in range(args.epoch):
         model.train_epoch(train_iter)
-        dev_acc = model.evaluate(dev_iter)
+        with torch.no_grad():
+            dev_acc = model.evaluate(dev_iter)
         patience -= 1
 
-        print(' \nEpoch {}, Patience : {}, Dev Acc : {:.2f}'.format(epoch,patience,dev_acc*100))
-        print(' \nEpoch {}, Patience : {}, Dev Acc : {:.2f}'.format(epoch,patience,dev_acc*100),file=logfile)
-        if patience > 0 and dev_acc > best_acc:
-            best_acc = dev_acc
+        print(' \nEpoch {}, Patience : {}, Dev Acc : ({:.2f},{:.2f})'.format(epoch,patience,dev_acc[0]*100,dev_acc[1]*100))
+        print(' \nEpoch {}, Patience : {}, Dev Acc : ({:.2f},{:.2f})'.format(epoch,patience,dev_acc[0]*100,dev_acc[1]*100),file=logfile)
+
+        if patience > 0 and dev_acc[0] > best_acc:
+            best_acc = dev_acc[0]
             torch.save(model.state_dict(),os.path.join(args.save_dir,'model.pth'))
             patience = args.patience
 
         if patience == 0:
             model = SimpleQA(args).to(device)
             model.load_state_dict(torch.load(os.path.join(args.save_dir,'model.pth')))
-            dev_acc = model.evaluate(dev_iter)
-            test_acc = model.evaluate(test_iter)
-            print('Dev Acc: {:.2f}, Test Acc :{:.2f}'.format(dev_acc*100,test_acc*100))
-            print('Dev Acc: {:.2f}, Test Acc :{:.2f}'.format(dev_acc*100,test_acc*100),file=logfile)
+            with torch.no_grad():
+                dev_acc = model.evaluate(dev_iter)
+                test_acc = model.evaluate(test_iter)
+            print('Dev Acc: ({:.2f},{:.2f}), Test Acc :({:.2f},{:.2f})'.format(dev_acc[0]*100,dev_acc[1]*100,test_acc[0]*100,test_acc[1]*100))
+            print('Dev Acc: ({:.2f},{:.2f}), Test Acc :({:.2f},{:.2f})'.format(dev_acc[0]*100,dev_acc[1]*100,test_acc[0]*100,test_acc[1]*100),file=logfile)
             logfile.close()
             return test_acc
 
 
-class DefaultConfig:
-    def __init__(self):
-        self.word_dim = 300
-        self.hidden_dim = 150
-        self.relation_dim = 300
-        self.batch_size = 64
-        self.epoch = 100
-        self.data_dir = None
-        self.lr = 1e-3
-        self.margin = 0.1
-        self.ns = 256
-        self.patience = 5
+def evaluate(args,test_dataset,vocab,collate_fn):
 
-        self.data_dir = './data/SimpleQuestions/10-fold-dataset-tsv'
-        self.vocab_pth = os.path.join(self.data_dir,'vocab.pth')
-        self.relation_file = os.path.join(self.data_dir,'relation.id')
-        self.word_pretrained_pth = os.path.join(self.data_dir,'word_pretrained.pth')
-
-        self.freeze = True
-        self.num_bases = 100
-        self.num_hidden_layers = 4
-        self.rgcn_dropout = 0.0
-
-        self.padding_idx = 0
-        self.pad_token = '<pad>'
-        self.unk_idx = 1
-        self.unk_token = '<unk>'
-
-        self.save_dir = 'results/simpleQA/gcn'
-
-        self.glove_pth = '/home/user_data/lijh/data/english_embeddings/glove.6B.300d.txt'
-        self.relation_vec_pth = './data/SimpleQuestions/relation2vec.txt'
-
-        self.relation_pretrained_pth = './data/SimpleQuestions/relation_pretrained.pth'
-        self.relation_adj_matrix_pth = './data/SimpleQuestions/relation_adj_matrix.pth'
-
-        self.threshold = 10
-
-        import pprint
-        pprint.pprint(self.__dict__)
+    test_iter = DataLoader(dataset=test_dataset,batch_size=args.batch_size,shuffle=True,num_workers=12,collate_fn=collate_fn)
+    args.n_words = len(vocab.stoi)
+    args.n_relations = len(vocab.rtoi)
+    args.padding_idx = 0
+    model = SimpleQA(args).to(device)
+    model.load_state_dict(torch.load(os.path.join(args.save_dir,'model.pth')))
+    micro_acc,macro_acc = model.evaluate(test_iter)
+    return micro_acc,macro_acc
 
 
-class TestConfig:
-    def __init__(self):
-        self.word_dim = 3
-        self.hidden_dim = 3
-        self.relation_dim = 6
-        self.batch_size = 10
-        self.epoch = 5
-        self.data_dir = 'data/SimpleQA'
-        self.lr = 1e-3
-        self.ns = 4
-        self.padding_idx = 0
-        self.pad_token = '<pad>'
-        self.unk_idx = 1
-        self.unk_token = '<unk>'
-        self.relation_file = 'data/SimpleQA/FB2M.rel_voc.pickle'
-        self.vocab_pth = 'data/SimpleQA/vocab.pth'
+def visualize(args,labels,vocab,fname):
+    args.n_words = len(vocab.stoi)
+    args.n_relations = len(vocab.rtoi)
+    args.padding_idx = 0
+    model = SimpleQA(args).to(device)
+    model.load_state_dict(torch.load(os.path.join(args.save_dir,'model.pth')))
+    if not args.use_gcn:
+        relation_embedding = model.get_relation_embedding().detach().cpu().numpy()
+    else:
+        relation_embedding = model.rgcn.layers[0].embedding.weight.detach().cpu().numpy()
+        # relation_embedding = model.get_relation_embedding().detach().cpu().numpy()
 
-        self.train_dataset_pth = './data/SimpleQA/train.pkl'
-        self.dev_dataset_pth = './data/SimpleQA/dev.pkl'
-        self.test_dataset_pth = './data/SimpleQA/test.pkl'
-
-        self.word_pretrained_path = '/home/user_data/lijh/data/glove.6B.300d.txt'
-        self.relation_pretrained_path = None
+    # normalize
+    # relation_embedding -= np.mean(relation_embedding,axis=0)
+    np.savetxt('embedding.tsv',relation_embedding,delimiter='\t')
+    plot(relation_embedding,fname,labels)
+    array = [0 for i in range(len(vocab.rtoi))]
+    for rel,idx in vocab.rtoi.items():
+        array[idx] = (rel,labels[idx])
+    # np.savetxt('label.tsv',array,delimiter='\t')
+    with open('label.tsv','w') as f:
+        f.write('Relation'+'\t'+'label'+'\n')
+        for rel,label in array:
+            f.write(rel + '\t' + str(label) + '\n')
 
 
 if __name__ == '__main__':
-    import sys
-    if sys.argv[1] == '--supervised':
-        args = DefaultConfig()
-        args.mode = 'supervised'
-        args.data_dir = './data/SimpleQuestions'
-        args.vocab_pth = os.path.join(args.data_dir,'vocab.pth')
-        args.relation_file = os.path.join(args.data_dir,'relation.id')
-        args.word_pretrained_pth = os.path.join(args.data_dir,'word_pretrained.pth')
-        main(args)
-    elif sys.argv[1] == '--test':
-        args = TestConfig()
-        main(args)
-    elif sys.argv[1] == '--zero-shot':
-        args = DefaultConfig()
-        args.mode = 'zero-shot'
-        args.data_dir = './data/SimpleQuestions/10-fold-dataset-tsv'
-        args.vocab_pth = os.path.join(args.data_dir,'vocab.pth')
-        args.relation_file = os.path.join(args.data_dir,'relation.id')
-        args.word_pretrained_pth = os.path.join(args.data_dir,'word_pretrained.pth')
-        main(args)
-    elif sys.argv[1] == '--generate':
-        args = DefaultConfig()
+
+    args_parser = ArgumentParser()
+    args_parser.add_argument('--config_file','-c',default=None,type=str)
+    args_parser.add_argument('--generate',action="store_true",default=False,)
+    args_parser.add_argument('--train',action="store_true",default=False)
+    args_parser.add_argument('--evaluate',action="store_true",default=False)
+    args_parser.add_argument('--visualize',action="store_true",default=False)
+    args_parser.add_argument('--analysis',action="store_true",default=False)
+    args = parse_args(args_parser)
+    pprint(vars(args))
+
+    if args.generate:
         args.data_dir = os.path.join(args.data_dir,'base')
-        SimpleQADataset.generate_vocab(args)
-        SimpleQADataset.generate_embedding(args,device)
+        # SimpleQADataset.generate_vocab(args)
+        # SimpleQADataset.generate_embedding(args,device)
+        SimpleQADataset.generate_relation_embedding(args,device)
         SimpleQADataset.generate_graph(args,device)
+    elif args.train or args.evaluate or args.visualize:
+        main(args)
