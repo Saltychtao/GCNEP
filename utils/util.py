f0b9d3cd1fc410494a145ae8a6eacc909e5a1817
Saltychtao <lijh@nlp.nju.edu.cn>
2019-04-11 16:22:33 +0800

    add util function pairwise_distance

--- utils/util.py
+++ utils/util.py
@@ -23,6 +23,7 @@ def one_hot_to_labels(tensor,one_label=None):
         labels[idx].append(label)
     return labels
 
+
 def load_pretrained(filepath,vocab,dim,device,pad_idx):
 
     vecs = np.random.normal(0,1,(len(vocab),dim))
@@ -39,8 +40,30 @@ def load_pretrained(filepath,vocab,dim,device,pad_idx):
             vec = [float(f) for f in splited[1:]]
             vecs[vocab[word]] = vec
         print('Found word vectors: {}/{}'.format(cnt,len(vocab)))
-    tensor= torch.from_numpy(vecs)
+    tensor = torch.from_numpy(vecs)
     return tensor.float().to(device)
+
+
+def pairwise_distances(x, y=None):
+    '''
+    Input: x is a Nxd matrix
+           y is an optional Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+            if y is not given then use 'y=x'.
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
+    '''
+    x_norm = (x**2).sum(1).view(-1, 1)
+    if y is not None:
+        y_t = torch.transpose(y, 0, 1)
+        y_norm = (y**2).sum(1).view(1, -1)
+    else:
+        y_t = torch.transpose(x, 0, 1)
+        y_norm = x_norm.view(1, -1)
+    dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
+    # Ensure diagonal is zero if x=y
+    # if y is None:
+    #     dist = dist - torch.diag(dist.diag)
+    return torch.clamp(dist, 0.0, np.inf)
 
 
 def parse_args(parser):
